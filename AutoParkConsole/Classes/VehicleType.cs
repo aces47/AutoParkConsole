4f2103dc290c74e7d6748d6e8a8a217e1326af33
Арсений <ars.stank@gmail.com>
2021-06-27 13:14:09 +0300

    Make level 1 in autopark project.

---
+++ AutoParkConsole/Classes/VehicleType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoParkConsole.Classes
+{
+	class VehicleType
+	{
+		string _TypeName;
+		double _TaxCoefficient;
+
+		public VehicleType()
+		{
+
+		}
+
+		public VehicleType(string name,double taxCoefficient = 1d)
+		{
+			_TypeName = name;
+			_TaxCoefficient = taxCoefficient;
+		}
+
+		public string GetTypeName() => _TypeName;
+
+		public void SetTypeName(string name) => _TypeName = name;
+
+		public double GetTaxCoefficient() => _TaxCoefficient;
+
+		public void SetTaxCoefficient(double taxCoefficient) => _TaxCoefficient = taxCoefficient;
+
+		public void Display()
+		{
+			Console.WriteLine($"TypeName = {_TypeName}\nTaxCoeffiient = {_TaxCoefficient}");
+		}
+
+		public override string ToString() => $"{_TypeName},{_TaxCoefficient}";
+	}
+}
