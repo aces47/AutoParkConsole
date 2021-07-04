4032c52af6f3dfda5be3322ab9b3625c91931f0f
aces47 <ars.stank@gmail.com>
2021-07-05 01:22:50 +0300

    fix minor bugs

--- AutoParkConsole/Classes/Vehicle.cs
+++ AutoParkConsole/Classes/Vehicle.cs
@@ -1,88 +1,90 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AutoParkConsole.Classes
 {
-	internal class Vehicle : IComparable<Vehicle>
-	{
-		public int Id { get; }
-		public AbstractEngine AbstractEngine { get; }
-		public VehicleType VehicleType { get; }
-		public string ModelName { get; }
-		public string GosNumber { get; set; }
-		public int Weight { get; set; }
-		public int ManufactureYear { get; }
-		public int Mileage { get; set; }
-		public Color Color { get; set; }
-		public double TankCapacity { get; set; }
-		public List<Rent> Rents { get; set; }
+    internal class Vehicle : IComparable<Vehicle>
+    {
+        public int Id { get; }
+        public AbstractEngine AbstractEngine { get; }
+        public VehicleType VehicleType { get; }
+        public string ModelName { get; }
+        public string GosNumber { get; set; }
+        public int Weight { get; set; }
+        public int ManufactureYear { get; }
+        public int Mileage { get; set; }
+        public Color Color { get; set; }
+        public double TankCapacity { get; set; }
+        public List<Rent> Rents { get; set; }
 
-		public Vehicle() { }
+        public Vehicle() { }
 
-		public Vehicle(
-			AbstractEngine engine,
-			VehicleType type,
-			string model,
-			string gosNumber,
-			int weight,
-			int manufactureYear,
-			int mileage,
-			Color color,
-			int tankCapacity = 0)
-		{
-			AbstractEngine = engine;
-			VehicleType = type;
-			ModelName = model;
-			GosNumber = gosNumber;
-			Weight = weight;
-			ManufactureYear = manufactureYear;
-			Mileage = mileage;
-			Color = color;
-			TankCapacity = tankCapacity;
-			Id = id;
-		}
+        public Vehicle(
+            int id,
+            AbstractEngine engine,
+            VehicleType type,
+            string model,
+            string gosNumber,
+            int weight,
+            int manufactureYear,
+            int mileage,
+            Color color,
+            int tankCapacity = 0)
+        {
+            AbstractEngine = engine;
+            VehicleType = type;
+            ModelName = model;
+            GosNumber = gosNumber;
+            Weight = weight;
+            ManufactureYear = manufactureYear;
+            Mileage = mileage;
+            Color = color;
+            TankCapacity = tankCapacity;
+            Id = id;
+        }
 
-		public decimal GetTotalVehicleIncome()
-		{
-			if (Rents != null)
-			{
-				decimal totalIncome = 0;
-				foreach (Rent rent in Rents)
-				{
-					totalIncome += rent.RentCost;
-				}
+        public decimal GetTotalVehicleIncome()
+        {
+            if (Rents != null)
+            {
+                decimal totalIncome = 0;
+                foreach (Rent rent in Rents)
+                {
+                    totalIncome += rent.RentCost;
+                }
 
-				return totalIncome;
-			}
+                return totalIncome;
+            }
 
-			return 0m;
-		}
+            return 0m;
+        }
 
-		public double GetCalcTaxPerMonth() =>
-			(Weight * 0.0013d) + (VehicleType.TaxCoefficient * AbstractEngine.TaxCoefficientByEngineType * 30) + 5;
+        public double GetCalcTaxPerMonth() =>
+            (Weight * 0.0013d) + (VehicleType.TaxCoefficient * AbstractEngine.TaxCoefficientByEngineType * 30) + 5;
 
-		public override bool Equals(object obj)
-		{
-			if (obj is Vehicle)
-			{
-				var other = obj as Vehicle;
-				if (VehicleType == other.VehicleType && ModelName.Equals(other.ModelName))
-				{
-					return true;
-				}
-			}
+        public override bool Equals(object obj)
+        {
+            if (obj is Vehicle)
+            {
+                var other = obj as Vehicle;
+                if (VehicleType == other.VehicleType && ModelName.Equals(other.ModelName))
+                {
+                    return true;
+                }
+            }
 
-			return false;
-		}
-		public override string ToString() =>
-			$"{AbstractEngine},{VehicleType},{ModelName},{GosNumber},{Weight},{ManufactureYear},{Mileage},{Color},{TankCapacity}";
+            return false;
+        }
+        public override string ToString() =>
+            $"{AbstractEngine},{VehicleType},{ModelName},{GosNumber},{Weight},{ManufactureYear},{Mileage},{Color},{TankCapacity}";
 
-		public int CompareTo(Vehicle other)
-		{
-			double thisTaxCoefficient = VehicleType.TaxCoefficient;
-			double otherTaxCoefficient = other.VehicleType.TaxCoefficient;
+        public int CompareTo(Vehicle other)
+        {
+            double thisTaxCoefficient = VehicleType.TaxCoefficient;
+            double otherTaxCoefficient = other.VehicleType.TaxCoefficient;
 
-			return thisTaxCoefficient.CompareTo(otherTaxCoefficient);
-		}
+            return thisTaxCoefficient.CompareTo(otherTaxCoefficient);
+        }
 
-	}
+    }
 }
